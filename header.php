19aefeb82645a49124294f229891a90bf51e9f98
Yeechan Lu <wz.bluesnow@gmail.com>
2013-12-12 05:23:01 +0800

    Move comments into PHP tag

--- header.php
+++ header.php
@@ -18,7 +18,8 @@
 <link rel="alternate" type="application/atom+xml" title="ATOM 1.0" href="<?php bloginfo('atom_url')?>" />
 
 <!--[if lt IE 9]>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"> // IE HTML 兼容 </script>
+	<?php // IE HTML 兼容  ?>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 
 <?php wp_head(); ?>
